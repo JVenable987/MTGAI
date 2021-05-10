b56aee227139721c168faaacfc48da842f80c710
JVenable987 <joshua.j.venable@gmail.com>
2021-05-10 03:34:43 -0600

    Updated with commented example
    Updated with examples from https://adventuresinmachinelearning.com/reinforcement-learning-tutorial-python-keras/

--- src/gym_MTG.py
+++ src/gym_MTG.py
@@ -292,3 +292,110 @@ class Deck:
         for i in range(1, 8):
             card_arr[self.DrawCard() - 1] += 1
         return card_arr
+        
+#Main function
+#Create the ai layers
+#apparently softmax is a good option for multiple inputs
+#to get the actual output from the AI
+#output = np.argmax(A2.T[index])
+#which outputs the highest probability number.
+#Q learning is different though
+#examples are directly from website at https://adventuresinmachinelearning.com/reinforcement-learning-tutorial-python-keras/
+#We should implement the keras one, but I have included others, in order to understand what's going on.
+
+#def eps_greedy_q_learning_with_table(env, num_episodes=500):
+#    q_table = np.zeros((5, 2))
+#    y = 0.95
+#    eps = 0.5
+#    lr = 0.8
+#    decay_factor = 0.999
+#    for i in range(num_episodes):
+#        s = env.reset()
+#        eps *= decay_factor
+#        done = False
+#        while not done:
+#            # select the action with highest cummulative reward
+#            if np.random.random() < eps or np.sum(q_table[s, :]) == 0:
+#                a = np.random.randint(0, 2)
+#            else:
+#                a = np.argmax(q_table[s, :])
+#            # pdb.set_trace()
+#            new_s, r, done, _ = env.step(a)
+#            q_table[s, a] += r + lr * (y * np.max(q_table[new_s, :]) - q_table[s, a])
+#            s = new_s
+#    return q_table
+
+#def test_methods(env, num_iterations=100):
+#    winner = np.zeros((3,))
+#    for g in range(num_iterations):
+#        m0_table = naive_sum_reward_agent(env, 500)
+#        m1_table = q_learning_with_table(env, 500)
+#        m2_table = eps_greedy_q_learning_with_table(env, 500)
+#        m0 = run_game(m0_table, env)
+#        m1 = run_game(m1_table, env)
+#        m2 = run_game(m2_table, env)
+#        w = np.argmax(np.array([m0, m1, m2]))
+#        winner[w] += 1
+#        print("Game {} of {}".format(g + 1, num_iterations))
+#    return winner
+
+#def run_game(table, env):
+#    s = env.reset()
+#    tot_reward = 0
+#    done = False
+#    while not done:
+#        a = np.argmax(table[s, :])
+#        s, r, done, _ = env.step(a)
+#        tot_reward += r
+#    return tot_reward
+
+#With keras model it is
+
+#model = Sequential()
+#model.add(InputLayer(batch_input_shape=(1, 5)))
+#model.add(Dense(10, activation='sigmoid'))
+#model.add(Dense(2, activation='linear'))
+#model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+
+# now execute the q learning
+#y = 0.95
+#eps = 0.5
+#decay_factor = 0.999
+#r_avg_list = []
+#for i in range(num_episodes):
+#    s = env.reset()
+#    eps *= decay_factor
+#    if i % 100 == 0:
+#        print("Episode {} of {}".format(i + 1, num_episodes))
+#    done = False
+#    r_sum = 0
+#    while not done:
+#        if np.random.random() < eps:
+#            a = np.random.randint(0, 2)
+#        else:
+#            a = np.argmax(model.predict(np.identity(5)[s:s + 1]))
+#        new_s, r, done, _ = env.step(a)
+#        target = r + y * np.max(model.predict(np.identity(5)[new_s:new_s + 1]))
+#        target_vec = model.predict(np.identity(5)[s:s + 1])[0]
+#        target_vec[a] = target
+#        model.fit(np.identity(5)[s:s + 1], target_vec.reshape(-1, 2), epochs=1, verbose=0)
+#        s = new_s
+#        r_sum += r
+#    r_avg_list.append(r_sum / 1000)
+
+#Training
+
+#not sure if it is
+#env = gym.make(MTGEnv)
+#env = gyme.make('MTGEnv')
+#or
+#env = MTGEnv()
+#for _ in range(1000):
+    #env.render()
+#    observation = env.reset()
+#    print(observation)
+#    print(output)
+#    env.step(env.action_space.sample())#the env.action_space.sample() should be replaced with the np.argmax I believe
+#    env.close()
+    
+#prediction
